df7bcdc2b6a824dcf4a07d1c22d35a6408a67f2b
Huybin1234b <officalhuybin1234b@duck.com>
2024-11-15 17:23:26 +0700

    changing the info select UI (in HTML)

---
+++ thisisthejsfile.js
@@ -0,0 +1,16 @@
+/* function DeathOrAliveSwitcher() {
+    var x = document.getElementById("DeathOrAliveSwitcher");
+    if (x.style.display === "none") {
+        
+    } 
+
+}
+*/
+/*
+function DarkOrLight-Mode() {
+    var x = document.getElementById("DarkOrLight-Mode");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+}
+*/ 
